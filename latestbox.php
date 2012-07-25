ab9474d586af8512ea9d3c7676141386173ee7f0
iv.dimova <iv.dimova@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-25 07:27:50 +0000

    Adding first version of Latest Box
    git-svn-id: https://plugins.svn.wordpress.org/latest-box/trunk@576996 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ latestbox.php
@@ -0,0 +1,65 @@
+<?php   
+    /* 
+    Plugin Name: Latest Posts Box
+    Plugin URI: http://it.ivdimova.com
+    Description: Plugin for displaying latest posts in a simple widget box
+    Author: ivdimova
+    Version: 1.0 
+    Author URI: http://it.ivdimova.com
+    */  
+    
+class LBoxWidget extends WP_Widget {
+    function LBoxWidget() {
+        parent::WP_Widget(false, $name = 'Latest Posts Box');
+    }
+    function form($instance) {
+        $title = esc_attr($instance['title']);
+        $num_posts = esc_attr($instance['num_posts']);
+        ?>
+            <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></label></p>
+            <p><label for="<?php echo $this->get_field_id('num_posts'); ?>"><?php _e('Number of Posts Displayed:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('num_posts'); ?>" name="<?php echo $this->get_field_name('num_posts'); ?>" type="text" value="<?php echo $num_posts; ?>" /></label></p>
+        <?php
+    }
+    function widget($args, $instance) {
+    extract( $args );
+    $title = apply_filters('widget_title', $instance['title']);
+    $num_posts = $instance['num_posts'];
+   echo $before_widget; 
+          if ( $title )
+            echo $before_title . $title . $after_title; ?>
+ 
+        <div class="lbox" style="width=200px; height=100px; border: 1px solid black;">
+           
+        <?php global $post;
+        $args = array( 'numberposts' => $num_posts);
+        $lposts = get_posts( $args );
+        foreach( $lposts as $post ) : setup_postdata($post); ?>
+       
+       
+       <li><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(40,40)); ?></a></li>
+        <?php endforeach; ?>
+
+ </div>
+         <?php echo $after_widget; 
+
+    }
+     }
+    
+    add_action('widgets_init', create_function('', 'return register_widget("LBoxWidget");')); 
+    
+    
+    function lbox_shtc ($args, $instance) {?>
+       
+           
+        <?php global $post;
+        $lposts = get_posts( $args );
+        foreach( $lposts as $post ) : setup_postdata($post); ?>
+       
+       
+       <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(40,40)); ?></a>
+        <?php endforeach; 
+
+
+  }
+   add_shortcode ('lbox','lbox_shtc');
+?>
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Latest Box ===
+Contributors: ivdimova
+Tags: latest posts, images 
+Stable tag: 1.0.
+Requires at least: 3.0
+Tested up to: 3.4
+License: GPLv2 or later
+
+==Description==
+Simple plugin that allows to the user to show a box containing featured images of recent posts. 
+Can be used like widget or short code 
+
+== Installation ==
+Upload the LatestBox plugin to your blog and activate - that's all.
+
+The widget offrers you option how many posts to be displayed. 
+
+If you need a shortcode usage, here is the snippet:
+
+[lbox]
